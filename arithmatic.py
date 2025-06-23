5b8c5273882bd634d8055d2efd4d29df480f2c99
Shadman <Shadmanwali444@gmail.com>
2025-06-23 22:00:28 +0600

    first commit

---
+++ arithmatic.py
@@ -0,0 +1,34 @@
+
+
+
+length_of_rectangle=float(input("Enter the length of the rectangle: "))
+width_of_rectangle =float (input("Enter the width of the rectangle: "))
+area = length_of_rectangle * width_of_rectangle
+perimeter = 2* (length_of_rectangle + width_of_rectangle)
+
+print("Area of the rectangle:", area)
+print("Perimeter of the rectangle:", perimeter)
+#2
+celsius = float(input("converting celsius to farhenheit: "))
+fahrenheit = (celsius * 9/5) + 32
+print("\n Celsius to Fahrenheit:", fahrenheit)
+
+# Task 3:
+
+remainder = 25 % 4
+print("\n Remainder of 25 ÷ 4:", remainder)
+
+# Task 4:
+result = (2 ** 3) * 5 + 0 / 1
+print("\n   Result of 2³ × 5 + 0 ÷ 1:", result)
+
+# Task 5: Average of 5 Numbers
+num1 = 50
+num2 = 60
+num3 = 75
+num4 = 80
+num5 = 90
+average = (num1 + num2 + num3 + num4 + num5) / 5
+print("\n Average of the  5 numbers:", average)
+
+
